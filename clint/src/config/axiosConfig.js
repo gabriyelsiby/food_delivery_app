ab3ca8b0510cb8151dc3cf58f7fe8fbcc9627c6d
gabriyelsiby <gabriyel952003@gmail.com>
2025-03-30 19:12:50 +0530

    axios fixed

--- clint/src/config/axiosConfig.js
+++ clint/src/config/axiosConfig.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
-export const axiosInstance = axios.create({
-    baseURL: `${API_URL}/api`,
-    withCredentials: true, // Ensures cookies are sent (JWT Auth)
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
+export const axiosInstance = axios.create({ baseURL: `${API_URL}/api`, withCredentials: true });
 
-export default axiosInstance;
