88e277d89d563313272f2f4e99dddb888352f290
gabriyelsiby <gabriyel952003@gmail.com>
2025-04-21 00:22:22 +0530

    push

--- client-user/src/config/axiosInstance.jsx
+++
@@ -1,9 +0,0 @@
-// config/axiosInstance.jsx
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL|| "https://food-delivery-app-server-sooty.vercel.app/api",
-  withCredentials: true,
-});
-
-export default axiosInstance;
